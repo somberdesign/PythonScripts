a100ed3505362957d4c0579c8b5c69911cc0cdf8
whitley <bobwhitleywork@gmail.com>
2020-10-13 08:49:26 -0500

    Select only image files

--- PickRandomFilename.py
+++
@@ -1,7 +0,0 @@
-import os
-import random
-import pyperclip
-
-dir = os.path.dirname(os.path.realpath(__file__))
-filename = random.choice(os.listdir(dir))
-pyperclip.copy(os.path.join(dir, filename))
---
+++ _PickRandomFilename.py
@@ -0,0 +1,13 @@
+import os
+import random
+import pyperclip
+
+goodFileExtension = ['.jpg', '.png']
+
+dir = os.path.dirname(os.path.realpath(__file__))
+filename = __file__
+
+while os.path.splitext(filename)[1] not in goodFileExtension:
+    filename = random.choice(os.listdir(dir))
+    
+pyperclip.copy(os.path.join(dir, filename))

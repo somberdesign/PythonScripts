c0fcf67e855ecc57af2257f2d8e8b5b8368cf33c
Bob Whitley <bobwhitley+github@gmail.com>
2022-02-09 19:33:57 -0600

    SeriesManager

--- SeriesManager/SeriesManager.py
+++ SeriesManager/SeriesManager.py
@@ -1,6 +1,8 @@
 
 import sqlite3, random, math, PySimpleGUI as sg, SeriesManager_Data as data, contextlib
 from tkinter import *
+import datetime
+import os
 
 FILENAME_DB = "titles.db"
 FONT_LABEL = ("Helvetica", 10, "bold")
@@ -8,6 +10,7 @@ KEY_LISTBOX_ACTIVETITLES = "__ACTIVETITLES__"
 KEY_LISTBOX_INACTIVETITLES = "__INACTIVETITLES__"
 KEY_DISPLAYTEXT = "__DISPLAYTEXT__"
 LASTVIEWEDDATE_DISPLAY_COLUMN_WIDTH = 10
+LOGFILE = os.path.join(os.path.dirname(__file__), "SeriesManage.log")
 
 
 idx_title = 0
@@ -18,9 +21,12 @@ idx_finalMultiplier = 4
 idx_highTicketNumber = 5
 idx_titleId = 6
 
+activeRecordCount = None
+
 DisplayColumns = [idx_highTicketNumber, idx_titleMultiplier, idx_totalViews, idx_title, idx_lastDateViewed]
 
 def GetDisplayText(oData):
+	global activeRecordCount
 
 	def GetOutput(allRecordRows, maxColumnWidth):
 		displayText = ""
@@ -36,6 +42,7 @@ def GetDisplayText(oData):
 
 
 	allRecordRows = oData.GetDisplayTitles()
+	activeRecordCount = len(allRecordRows)
 	maxColumnWidth = [0, 0, 0, 0, 0, 0, 0]
 	PerformCalculations(allRecordRows, maxColumnWidth)
 
@@ -43,94 +50,94 @@ def GetDisplayText(oData):
 
 
 def GetAddSeriesWindow():
-    txtDisplay = sg.Text("", key=KEY_DISPLAYTEXT, font=("Courier New", 10), size=(50,1), text_color="red")
-    sldMultiplier = sg.Slider(range=(1,30), default_value=10, orientation="horizontal")
-    lblMultiplier = sg.Text("\nMultiplier", font=FONT_LABEL, size=(12,2))
-    inputText = sg.InputText()
-    lblTitle = sg.Text     ("Series Name", font=FONT_LABEL, size=(12,1))
-    inputText.SetFocus()
-    layout = [
-        [txtDisplay],
-        [lblTitle, inputText],
-        [lblMultiplier, sldMultiplier],
-        [],
-        [sg.Button('Ok'), sg.Button('Cancel')]
-        ]
-    
-    window = sg.Window("Add Series", layout)
+	txtDisplay = sg.Text("", key=KEY_DISPLAYTEXT, font=("Courier New", 10), size=(50,1), text_color="red")
+	sldMultiplier = sg.Slider(range=(1,30), default_value=10, orientation="horizontal")
+	lblMultiplier = sg.Text("\nMultiplier", font=FONT_LABEL, size=(12,2))
+	inputText = sg.InputText()
+	lblTitle = sg.Text	 ("Series Name", font=FONT_LABEL, size=(12,1))
+	inputText.SetFocus()
+	layout = [
+		[txtDisplay],
+		[lblTitle, inputText],
+		[lblMultiplier, sldMultiplier],
+		[],
+		[sg.Button('Ok'), sg.Button('Cancel')]
+		]
+	
+	window = sg.Window("Add Series", layout)
 
-    return window
+	return window
 
 
 def GetDisplayWindow():
-    btnLotteryDraw = sg.Button("Lottery Draw")
-    btnManageSeries = sg.Button("Manage Series")
-    btnAddSeries = sg.Button("Add Series")
-    btnExit = sg.Button("Exit")
-    txtDisplay = sg.Text(GetDisplayText(oData), key=KEY_DISPLAYTEXT, font=("Courier New", 10))
+	btnLotteryDraw = sg.Button("Lottery Draw")
+	btnManageSeries = sg.Button("Manage Series")
+	btnAddSeries = sg.Button("Add Series")
+	btnExit = sg.Button("Exit")
+	txtDisplay = sg.Text(GetDisplayText(oData), key=KEY_DISPLAYTEXT, font=("Courier New", 10))
 
-    layout = [
-			    [txtDisplay],
-                [btnLotteryDraw, btnAddSeries, btnManageSeries, btnExit]
-             ]
-    return sg.Window("Series Picker", layout)
+	layout = [
+				[txtDisplay],
+				[btnLotteryDraw, btnAddSeries, btnManageSeries, btnExit]
+			 ]
+	return sg.Window("Series Picker", layout)
 
 def GetManageSeriesWindow(oData):
-    
-    activeItems = oData.GetActiveTitles()
-    inactiveItems = oData.GetInactiveTitles()
-    
-    activeTitles = []
-    for row in activeItems:
-        activeTitles.append(row[idx_title])
+	
+	activeItems = oData.GetActiveTitles()
+	inactiveItems = oData.GetInactiveTitles()
+	
+	activeTitles = []
+	for row in activeItems:
+		activeTitles.append(row[idx_title])
 
-    inactiveTitles = []
-    for row in inactiveItems:
-        inactiveTitles.append(row[idx_title])
+	inactiveTitles = []
+	for row in inactiveItems:
+		inactiveTitles.append(row[idx_title])
 
-    cboActiveTitles = sg.Listbox(values=activeTitles, size=(30, 8), key=KEY_LISTBOX_ACTIVETITLES, enable_events=True)
-    txtMarkInactive = sg.Text("Active Items", font=FONT_LABEL, justification="center")
-    cboInactiveTitles = sg.Listbox(values=inactiveTitles, size=(30, 8), key=KEY_LISTBOX_INACTIVETITLES, enable_events=True)
-    txtMarkActive = sg.Text("Inactive Items", font=FONT_LABEL, justification="center")
-    btnDone = sg.Button("Done")
+	cboActiveTitles = sg.Listbox(values=activeTitles, size=(30, 8), key=KEY_LISTBOX_ACTIVETITLES, enable_events=True)
+	txtMarkInactive = sg.Text("Active Items", font=FONT_LABEL, justification="center")
+	cboInactiveTitles = sg.Listbox(values=inactiveTitles, size=(30, 8), key=KEY_LISTBOX_INACTIVETITLES, enable_events=True)
+	txtMarkActive = sg.Text("Inactive Items", font=FONT_LABEL, justification="center")
+	btnDone = sg.Button("Done")
 
-    activeLayout = [
-        [txtMarkInactive],
-        [cboActiveTitles]
-        ]
+	activeLayout = [
+		[txtMarkInactive],
+		[cboActiveTitles]
+		]
 
-    inactiveLayout = [
-        [txtMarkActive],
-        [cboInactiveTitles]
-        ]
-    layout = [
-            [sg.Column(activeLayout), sg.Column(inactiveLayout)],
-            [btnDone]
-        ]
+	inactiveLayout = [
+		[txtMarkActive],
+		[cboInactiveTitles]
+		]
+	layout = [
+			[sg.Column(activeLayout), sg.Column(inactiveLayout)],
+			[btnDone]
+		]
 
-    return sg.Window("Manage Series", layout)
+	return sg.Window("Manage Series", layout)
 
 def PerformAddSeries(oData):
-    addWindow = GetAddSeriesWindow()
-    doneWithSeries = False
+	addWindow = GetAddSeriesWindow()
+	doneWithSeries = False
 
-    while not doneWithSeries:
-        addEvent, addValues = addWindow.Read()
+	while not doneWithSeries:
+		addEvent, addValues = addWindow.Read()
 
-        if addEvent == "Ok":
-            title = addValues[0]
-            multiplier = addValues[1] / 10
-            seriesExists = oData.SeriesExists(title)
-            if seriesExists == True:
-                addWindow[KEY_DISPLAYTEXT]("That series already exists")
-            else:
-                oData.InsertTitle(title, multiplier)
-                addWindow.close()
-                return
+		if addEvent == "Ok":
+			title = addValues[0]
+			multiplier = addValues[1] / 10
+			seriesExists = oData.SeriesExists(title)
+			if seriesExists == True:
+				addWindow[KEY_DISPLAYTEXT]("That series already exists")
+			else:
+				oData.InsertTitle(title, multiplier)
+				addWindow.close()
+				return
 
-        if addEvent is None or addEvent == "Cancel":
-            addWindow.close()
-            return
+		if addEvent is None or addEvent == "Cancel":
+			addWindow.close()
+			return
 
 
 def PerformCalculations(allRecordRows, maxColumnWidth):
@@ -165,9 +172,7 @@ def PerformDraw(oData):
 	highestTicketNumber = math.floor(highestTicketNumber + highestTicketNumber * 0.1)
 
 	goldenTicket = random.randrange(highestTicketNumber)
-	# m = "highestTicketNumber=" + str(highestTicketNumber) + "\ngoldenTicket=" + str(goldenTicket)
-	# sg.Popup(m)
-	# print(m)
+	Log_Message(f'highestTicketNumber={highestTicketNumber}, goldenTicket={goldenTicket}')
 
 	selectedIdx = 0
 	outText = ""
@@ -181,50 +186,60 @@ def PerformDraw(oData):
 
 def PerformManageSeries(oData):
 
-    while True:
-        manageWindow = GetManageSeriesWindow(oData)
-        manageEvent, manageValues = manageWindow.Read()
+	while True:
+		manageWindow = GetManageSeriesWindow(oData)
+		manageEvent, manageValues = manageWindow.Read()
 
-        if manageEvent is None or manageEvent == "Done": # go back to main display window
-            manageWindow.close()
-            return
+		if manageEvent is None or manageEvent == "Done": # go back to main display window
+			manageWindow.close()
+			return
 
-        if len(manageValues[KEY_LISTBOX_ACTIVETITLES]) > 0:
-            oData.SetTitleIsActive(manageValues[KEY_LISTBOX_ACTIVETITLES][0], 0)
-            manageWindow.close()
+		if len(manageValues[KEY_LISTBOX_ACTIVETITLES]) > 0:
+			oData.SetTitleIsActive(manageValues[KEY_LISTBOX_ACTIVETITLES][0], 0)
+			manageWindow.close()
 
-        if len(manageValues[KEY_LISTBOX_INACTIVETITLES]) > 0:
-            oData.SetTitleIsActive(manageValues[KEY_LISTBOX_INACTIVETITLES][0], 1)
-            manageWindow.close()
+		if len(manageValues[KEY_LISTBOX_INACTIVETITLES]) > 0:
+			oData.SetTitleIsActive(manageValues[KEY_LISTBOX_INACTIVETITLES][0], 1)
+			manageWindow.close()
 
+def Log_Message(message:str):
+	with open(LOGFILE, 'a') as file:
+		file.write(f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} - {message}\n')
+	print(message)
 
 ###########################################################################################
 
+Log_Message('Executing Series Manager')
+
 with data.SeriesManager_Data() as oData:
 
-    displayWindow = GetDisplayWindow()
+	displayWindow = GetDisplayWindow()
 
-    while True:
+	while True:
 
-        event, values = displayWindow.Read()
+		event, values = displayWindow.Read()
+		
+		if isinstance(activeRecordCount, int):
+			displayWindow.TKroot.title = (f'Series Manager ({activeRecordCount} active titles')
 
-        if event is None or event == "Exit":
-            break
+		if event is None or event == "Exit":
+			break
 
-        if event == "Add Series":
-            displayWindow.close()
-            PerformAddSeries(oData)
-            displayWindow = GetDisplayWindow()
+		if event == "Add Series":
+			displayWindow.close()
+			PerformAddSeries(oData)
+			displayWindow = GetDisplayWindow()
 
-        if event == "Lottery Draw":
-            PerformDraw(oData)
-            displayWindow[KEY_DISPLAYTEXT](GetDisplayText(oData))
+		if event == "Lottery Draw":
+			PerformDraw(oData)
+			displayWindow[KEY_DISPLAYTEXT](GetDisplayText(oData))
 
-        if event == "Manage Series":
-            displayWindow.close()
-            PerformManageSeries(oData)
-            displayWindow = GetDisplayWindow()
+		if event == "Manage Series":
+			displayWindow.close()
+			PerformManageSeries(oData)
+			displayWindow = GetDisplayWindow()
 
+Log_Message('Terminated Series Manager')
 
 
 
--- SeriesManager/SeriesManager_Data.py
+++ SeriesManager/SeriesManager_Data.py
@@ -1,105 +1,111 @@
 import sqlite3, easygui, contextlib, datetime, os
 
 class SeriesManager_Data(object):
-    """Interaction with datasource"""
-    
-    FILENAME_DB = os.path.join(os.path.dirname(__file__), "titles.db")
-    SQL_GETINACTIVETITLES = """
-        SELECT 
-	        t.title
-	        ,COALESCE(use_count.total_views, 0) total_views
-	        ,COALESCE(use_date.last_date_viewed, DATE('1970-01-01')) last_date_viewed
-	        ,t.multiplier title_multiplier
-	        ,t.id title_id
-        FROM
-	        Titles t
-	        LEFT JOIN (SELECT title_id, COUNT(*) total_views FROM uses GROUP BY title_id) use_count ON t.id = use_count.title_id
-	        LEFT JOIN (SELECT title_id, MAX(datetime(date_used)) last_date_viewed FROM uses GROUP BY title_id) use_date ON t.id = use_date.title_id
-        WHERE t.is_active = 0
-    """
-    SQL_GETTITLES = """
-        SELECT 
-	        t.title
-	        ,COALESCE(use_count.total_views, 0) total_views
-	        ,COALESCE(use_date.last_date_viewed, DATE('1970-01-01')) last_date_viewed
-	        ,t.multiplier title_multiplier
-	        ,t.id title_id
-        FROM
-	        Titles t
-	        LEFT JOIN (SELECT title_id, COUNT(*) total_views FROM uses GROUP BY title_id) use_count ON t.id = use_count.title_id
-	        LEFT JOIN (SELECT title_id, MAX(datetime(date_used)) last_date_viewed FROM uses GROUP BY title_id) use_date ON t.id = use_date.title_id
-        WHERE t.is_active = 1
-    """
-    SQL_INSERTTITLE = "insert into Titles (title, is_active, multiplier) VALUES(?, 1, ?)"
-    SQL_INSERTUSE = "INSERT INTO Uses (title_id, date_used) VALUES({0}, '{1}')"
-    SQL_TITLEEXISTS = "SELECT CASE WHEN COUNT(*) = 1 THEN 1 ELSE 0 END titleExists FROM titles WHERE LOWER(title) = ?"
-    SQL_UPDATETITLE_SETISACTIVE = "UPDATE Titles SET is_active = {1} WHERE LOWER(title) = '{0}'"
+	"""Interaction with datasource"""
+	
+	FILENAME_DB = os.path.join(os.path.dirname(__file__), "titles.db")
+	LOGFILE = os.path.join(os.path.dirname(__file__), "SeriesManage.log")
+	SQL_GETINACTIVETITLES = """
+		SELECT 
+			t.title
+			,COALESCE(use_count.total_views, 0) total_views
+			,COALESCE(use_date.last_date_viewed, DATE('1970-01-01')) last_date_viewed
+			,t.multiplier title_multiplier
+			,t.id title_id
+		FROM
+			Titles t
+			LEFT JOIN (SELECT title_id, COUNT(*) total_views FROM uses GROUP BY title_id) use_count ON t.id = use_count.title_id
+			LEFT JOIN (SELECT title_id, MAX(datetime(date_used)) last_date_viewed FROM uses GROUP BY title_id) use_date ON t.id = use_date.title_id
+		WHERE t.is_active = 0
+	"""
+	SQL_GETTITLES = """
+		SELECT 
+			t.title
+			,COALESCE(use_count.total_views, 0) total_views
+			,COALESCE(use_date.last_date_viewed, DATE('1970-01-01')) last_date_viewed
+			,t.multiplier title_multiplier
+			,t.id title_id
+		FROM
+			Titles t
+			LEFT JOIN (SELECT title_id, COUNT(*) total_views FROM uses GROUP BY title_id) use_count ON t.id = use_count.title_id
+			LEFT JOIN (SELECT title_id, MAX(datetime(date_used)) last_date_viewed FROM uses GROUP BY title_id) use_date ON t.id = use_date.title_id
+		WHERE t.is_active = 1
+	"""
+	SQL_INSERTTITLE = "insert into Titles (title, is_active, multiplier) VALUES(?, 1, ?)"
+	SQL_INSERTUSE = "INSERT INTO Uses (title_id, date_used) VALUES({0}, '{1}')"
+	SQL_TITLEEXISTS = "SELECT CASE WHEN COUNT(*) = 1 THEN 1 ELSE 0 END titleExists FROM titles WHERE LOWER(title) = ?"
+	SQL_UPDATETITLE_SETISACTIVE = "UPDATE Titles SET is_active = {1} WHERE LOWER(title) = '{0}'"
 
-    def __enter__(self):
-        print(f'Using database {SeriesManager_Data.FILENAME_DB}')
-        self.conn = sqlite3.connect(SeriesManager_Data.FILENAME_DB)
-        return self
+	def __enter__(self):
+		self.Log_Message(f'Using database {SeriesManager_Data.FILENAME_DB}')
+		self.conn = sqlite3.connect(SeriesManager_Data.FILENAME_DB)
+		return self
 
-    def __exit__(self, exception_type, exception_value, traceback):
-        self.conn.commit()
-        self.conn.close()
+	def __exit__(self, exception_type, exception_value, traceback):
+		self.conn.commit()
+		self.conn.close()
 
-    def GetActiveTitles(self):
-        allRecordRows = []
+	def GetActiveTitles(self):
+		allRecordRows = []
 
-        for row in self.conn.execute(SeriesManager_Data.SQL_GETTITLES + " ORDER BY LOWER(t.title)"):
-            newItem = [row[0], row[1], row[2], row[3], 1, 0, row[4]]
-            allRecordRows.append(newItem)
+		for row in self.conn.execute(SeriesManager_Data.SQL_GETTITLES + " ORDER BY LOWER(t.title)"):
+			newItem = [row[0], row[1], row[2], row[3], 1, 0, row[4]]
+			allRecordRows.append(newItem)
 
-        return allRecordRows
+		return allRecordRows
 
-    def GetDisplayTitles(self):
-        allRecordRows = []
+	def GetDisplayTitles(self):
+		allRecordRows = []
 
-        for row in self.conn.execute(SeriesManager_Data.SQL_GETTITLES + " ORDER BY last_date_viewed DESC, total_views DESC"):
-            newItem = [row[0], row[1], row[2], row[3], 1, 0, row[4]]
-            allRecordRows.append(newItem)
+		for row in self.conn.execute(SeriesManager_Data.SQL_GETTITLES + " ORDER BY last_date_viewed DESC, total_views DESC"):
+			newItem = [row[0], row[1], row[2], row[3], 1, 0, row[4]]
+			allRecordRows.append(newItem)
 
-        return allRecordRows
+		return allRecordRows
 
-    def GetInactiveTitles(self):
-        allRecordRows = []
+	def GetInactiveTitles(self):
+		allRecordRows = []
 
-        for row in self.conn.execute(SeriesManager_Data.SQL_GETINACTIVETITLES + " ORDER BY LOWER(t.title)"):
-            newItem = [row[0], row[1], row[2], row[3], 1, 0, row[4]]
-            allRecordRows.append(newItem)
+		for row in self.conn.execute(SeriesManager_Data.SQL_GETINACTIVETITLES + " ORDER BY LOWER(t.title)"):
+			newItem = [row[0], row[1], row[2], row[3], 1, 0, row[4]]
+			allRecordRows.append(newItem)
 
-        return allRecordRows
+		return allRecordRows
 
-    def InsertUse(self, titleId):
-        sql = SeriesManager_Data.SQL_INSERTUSE.format(titleId, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-        with contextlib.closing(self.conn.cursor()) as cur:
-            cur.execute(sql)
-        return
+	def InsertUse(self, titleId):
+		sql = SeriesManager_Data.SQL_INSERTUSE.format(titleId, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+		with contextlib.closing(self.conn.cursor()) as cur:
+			cur.execute(sql)
+		return
 
 
-    def InsertTitle(self, title, multiplier):
-        with contextlib.closing(self.conn.cursor()) as cur:
-            cur.execute(SeriesManager_Data.SQL_INSERTTITLE, [title, multiplier])
-        return
+	def InsertTitle(self, title, multiplier):
+		with contextlib.closing(self.conn.cursor()) as cur:
+			cur.execute(SeriesManager_Data.SQL_INSERTTITLE, [title, multiplier])
+		return
 
-    def SeriesExists(self, seriesTitle):
-        with contextlib.closing(self.conn.cursor()) as cur:
-            resultRow = cur.execute(SeriesManager_Data.SQL_TITLEEXISTS, [seriesTitle.lower()]).fetchone()
+	def SeriesExists(self, seriesTitle):
+		with contextlib.closing(self.conn.cursor()) as cur:
+			resultRow = cur.execute(SeriesManager_Data.SQL_TITLEEXISTS, [seriesTitle.lower()]).fetchone()
 
-        return True if resultRow[0] == 1 else False
+		return True if resultRow[0] == 1 else False
 
-    def SetTitleIsActive(self, title, isActive):
-        title = self.ScrubString(title)
-        with contextlib.closing(self.conn.cursor()) as cur:
-            try:
-                cur.execute(SeriesManager_Data.SQL_UPDATETITLE_SETISACTIVE.format(title.lower(), isActive))
-            except Exception as ex:
-                print(f'Exception in SetTitleIsActive({title}, {isActive}): {ex}')
-        return
+	def SetTitleIsActive(self, title, isActive):
+		title = self.ScrubString(title)
+		with contextlib.closing(self.conn.cursor()) as cur:
+			try:
+				cur.execute(SeriesManager_Data.SQL_UPDATETITLE_SETISACTIVE.format(title.lower(), isActive))
+			except Exception as ex:
+				self.Log_Message(f'Exception in SetTitleIsActive({title}, {isActive}): {ex}')
+		return
 		
-    def ScrubString(self, instring):
-        return instring.replace("'", "''")
+	def ScrubString(self, instring):
+		return instring.replace("'", "''")
+		
+	def Log_Message(self, message:str):
+		with open(self.LOGFILE, 'a') as file:
+			file.write(f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} - {message}\n')
+		print(message)
 
-        
+		
 

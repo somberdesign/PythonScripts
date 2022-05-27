6d0b0f3464a5aedb8d0c0828219b31519aca8456
Bob W <rgw3@hotmail.com>
2022-05-27 09:01:13 -0500

    CountFilesPerDir: Add command line arguments

--- CountFilesPerDir/CountFilesPerDir.py
+++ CountFilesPerDir/CountFilesPerDir.py
@@ -9,48 +9,7 @@ import sys
 TARGET_FILETYPE = 'url'
 SPACES = ' ' * 15
 
-def GetDirDate(dirstring):
-	returnVal = dt(1970, 1, 1)
-	parts = dirstring.split('-')
-	
-	if (
-		len(parts) != 4
-		or not RepresentsInt(parts[0])
-		or not RepresentsInt(parts[1])
-		or not RepresentsInt(parts[2])
-		):
-		return returnVal
-		
-	try:
-		returnVal = dt(int(parts[0]), int(parts[1]), int(parts[2]))
-	except Exception as ex:
-		return returnVal
-		
-	return returnVal
-
-def RepresentsInt(s):
-    try: 
-        int(s)
-        return True
-    except ValueError:
-        return False	
-
-if __name__ == '__main__':
-
-	# get the number of parameters passed in
-	argc = len(sys.argv)
-	
-	# verify corrrect number of params
-	if argc < 2:
-		print('USAGE: py CountFilesPerDir.py <targetDir>')
-		exit(1)
-
-	# verify param is an xml file
-	targetDir = sys.argv[1]
-	if not os.path.isdir(targetDir):
-		print(f'Invalid directory ({targetDir})')
-		exit(1)
-
+def CountDirectories(targetDir:str):
 	results = {}
 	# loop over date dirs
 	for dir in [d for d in os.listdir(targetDir) if os.path.isdir(os.path.join(targetDir, d))]:
@@ -99,5 +58,77 @@ if __name__ == '__main__':
 	# for item in [v[0] for v in sorted(results.items(), key=lambda kv: (-kv[1][0], kv[0]))]:
 	for item in results.items():
 		print(f'{item[0]}    {item[1][0] if item[1][0] > 0 else " "}       {item[1][1] if item[1][1] > 0 else " "} ')
+
+def CountFiles(targetDir:str):
+	pass
+
+def ProcessCommandLine():
+	returnVal = [str(), []]
+	validArguments = ['countfiles']
+
+	# get the number of parameters passed in
+	argc = len(sys.argv)
+	
+	# verify corrrect number of params
+	if argc < 2:
+		print('USAGE: py CountFilesPerDir.py <targetDir>')
+		exit(1)
+
+	# verify param is an xml file
+	targetDir = sys.argv[1]
+	if not os.path.isdir(targetDir):
+		print(f'Invalid directory ({targetDir})')
+		exit(1)
+	returnVal[0] = targetDir
+
+	if argc > 2:
+		for i in range(2..argc):
+			arg = sys.argv[i]
+			if len(arg) < 3 or arg != '--' or arg[2:].lower() not in validArguments:
+				print(f'Invalid argument: {arg}')
+				continue
+			returnVal[1].append(arg)
+
+	return returnVal
+
+
+def GetDirDate(dirstring):
+	returnVal = dt(1970, 1, 1)
+	parts = dirstring.split('-')
+	
+	if (
+		len(parts) != 4
+		or not RepresentsInt(parts[0])
+		or not RepresentsInt(parts[1])
+		or not RepresentsInt(parts[2])
+		):
+		return returnVal
+		
+	try:
+		returnVal = dt(int(parts[0]), int(parts[1]), int(parts[2]))
+	except Exception as ex:
+		return returnVal
+		
+	return returnVal
+
+def RepresentsInt(s):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False	
+
+if __name__ == '__main__':
+	
+
+	arguments = ProcessCommandLine()
+	targetDir = arguments[0]
+
+	if 'countfiles' in arguments[1]:
+		CountFiles(targetDir)
+
+	else:
+		CountDirectories(targetDir)
+
 		
 	input()

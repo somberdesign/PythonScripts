010ac85b3a9755020ec7e8177976ac4304970372
Bob W <rgw3@hotmail.com>
2022-07-25 08:14:45 -0500

    SearchToBrowser: Add cd support

--- FileList/Albumlist.py
+++ FileList/Albumlist.py
@@ -4,6 +4,8 @@ from glob import glob
 from os import path
 
 ALBUM_DIR = r'h:\Music\Albums'
+OUTPUT_FILE = r'\Users\Bob\PythonScripts\SearchToBrowser\AlbumList.txt'
+
 
 albumlist = []
 for dir in glob(path.join(ALBUM_DIR, '*', '*')):
@@ -11,9 +13,10 @@ for dir in glob(path.join(ALBUM_DIR, '*', '*')):
 
 albumlist = sorted(albumlist, key=str.casefold)
 
-with open('albumlist.txt', 'w') as f:
+with open(OUTPUT_FILE, 'w') as f:
     f.write(f'{date.today()}\n')
-    f.write(f'This file created by {__file__}\n\n')
+    f.write(f'This file created by {__file__}\n')
+    f.write(f'Wrote {len(albumlist)} items\n\n')
     for line in albumlist:
         f.write(f"{line}\n")
 

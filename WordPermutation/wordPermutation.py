1845b0cc06bbb6c3040e58ad4475a8f4bb896064
Bob Whitley <bobwhitley@gmail.com>
2024-02-19 18:50:44 -0600

    WordPermutation: continued development

--- WordPermutation/wordPermutation.py
+++ WordPermutation/wordPermutation.py
@@ -1,6 +1,11 @@
 from itertools import permutations
 import pyperclip
+import random
+import re
 import sys
+
+
+WORD_LIMIT = 6
 
 def generate_word_permutations(words):
     # Generate all permutations of the words
@@ -18,17 +23,37 @@ if __name__ == '__main__':
         print('No words')
         exit(0)
 
+
     word_list = []
     for i in range(1, len(sys.argv)):
-        word_list.append(sys.argv[i])
+        word_list.append(re.sub("[^A-z0-9]", "", sys.argv[i].lower().strip()))
+
+    
+    for w in ['by', 'the', 'a', 'an', 'to', 'of']:
+        while w in word_list:
+            word_list.remove(w)
+
+    if len(word_list) > WORD_LIMIT:
+        msg = f'Found {len(sys.argv) - 1} words. Limit is {WORD_LIMIT}'
+        print(msg)
+        pyperclip.copy(msg)
+        exit(0)
 
     result = generate_word_permutations(word_list)
 
-    # Print the result
-    output = "<div style=""color: white"">"
+    r = lambda: random.randint(0,255)
+    color = '#{:02x}{:02x}{:02x}'.format(r(), r(), r())
+
+    content = ''
     for idx, permutation in enumerate(result, start=1):
-        output += permutation + " "
-    output += "</div>"
+        content += permutation + " "
+
+    height = '10px'
+    if len(content) > 31000:
+        height = '12px'
+
+    output = f'<div style="color: {color}; background-color: {color}; font-size: 1px; padding: 15px 0px 15px 0px; height: {height}">{content}</div>'
 
     pyperclip.copy(output)
+    print(output)
     

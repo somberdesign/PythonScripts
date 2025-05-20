d385dadf385ae0fac0bf3a6e006032bc518c5baf
Bob Whitley <bobwhitley@gmail.com>
2025-05-20 18:31:06 -0500

    SearchToBrowser: add MovieSales.txt info (and random font)

--- SearchToBrowser/SearchToBrowser.py
+++ SearchToBrowser/SearchToBrowser.py
@@ -8,20 +8,22 @@ import re
 import string
 import sys
 from time import time
-from typing import Tuple
+from typing import Tuple, List
 from subprocess import call
 import easygui
+from random import choice
 
 DIR_TO_SEARCH = r'c:\Users\rgw3\PythonScripts\SearchToBrowser'
 OUTPUT_DIRECTORY = r'c:\temp\searchToBrowser'
 EVERYTHING_COMMAND_LINE_PATH = r'"C:\Program Files\Everything\es.exe"' # leave empty to disable
+SALES_FILE_PATH = r'h:\Cached\MovieList_Sales.txt'
 DEBUG = False
 
 def CleanText(line:str) -> str:
 	returnVal = re.sub('[^A-Za-z0-9 \n\-\~]', str(), line)
 	return returnVal
 	
-def GetArguments(configValues:dict) -> Tuple:
+def GetArguments(configValues:dict[str, str]) -> Tuple[bool, str]:
 
 	# get the number of parameters passed in 
 	argc = len(sys.argv)
@@ -53,9 +55,29 @@ def GetArguments(configValues:dict) -> Tuple:
 			warningMessage += f'{v} has is not defined\n'
 
 	if len(warningMessage) > 0:
-		easygui(f'WARNING\n{warningMessage}')
+		easygui.msgbox(f'WARNING\n{warningMessage}')
 
 	return True, str()
+
+def GetGoogleFontName() -> str:
+	fontnames:List[str] = ['Roboto', 'Lora', 'Pacifico', 'Montserrat', 'Raleway', 'Oswald', 'Open', 'Playfair', 'Merriweather', 'Caveat', 'Nunito', 'Abril', 'Comfortaa', 'Indie', 'Anton', 'Quicksand', 'Libre', 'Barlow', 'Exo', 'Arvo', 'Amatic', 'Dancing', 'Josefin', 'Fira', 'Bitter', 'Patua', 'Ubuntu', 'Satisfy', 'Zilla', 'Alegreya', 'Cinzel', 'DM', 'Spectral', 'Shadows', 'PT', 'Slabo', 'Teko', 'Yanone', 'Source', 'Archivo', 'Volkhov', 'Cormorant', 'Cardo', 'Just', 'Francois', 'Fredoka', 'Kaushan', 'Sacremento', 'Chivo', 'Bangers', 'Permanent', 'Crimson', 'Overpass', 'Orbitron', 'Manrope', 'Varela', 'Fjalla', 'Rokkitt', 'Hind', 'Rock', 'Baloo', 'Maven', 'Work', 'Architects', 'Saira', 'Righteous', 'Press', 'Megrim', 'Telex', 'Cantata', 'Staatliches', 'Titan', 'Kumar', 'Economica', 'Averia', 'Noto', 'Yeseva', 'Alice', 'Handlee', 'Mukta', 'Bevan', 'Luckiest', 'Anton', 'Koulen', 'Sen', 'Tangerine', 'Corben', 'Armata', 'Julee', 'Epilogue', 'Special', 'M', 'Lexend', 'Marcellus', 'Asap', 'Vibur', 'Ewert', 'Anonymous', 'Ultra', 'Belanosima']
+	return choice(fontnames)
+
+
+def GetSalesFileLines(searchterm:str) -> List[str]:
+	returnVal:List[str] = []
+	searchLines:List[str] = []
+
+	with open(SALES_FILE_PATH, 'r') as f:
+		searchLines = f.readlines()
+
+	for line in searchLines:
+		result = re.search(r'^\d*\-\d{3}', line)
+		if result is not None and searchterm.lower() in line.lower(): # type: ignore
+			returnVal.append(line)
+
+	return returnVal
+
 
 
 if __name__ == '__main__':
@@ -70,6 +92,8 @@ if __name__ == '__main__':
 		ctypes.windll.user32.MessageBoxW(0, result[1])
 		exit(0)
 
+	# get sales info
+	salesFileLines:List[str] = GetSalesFileLines(configValues['searchterm'])
 
 
 	pattern = re.compile('[/W_]+') # non-alphanumeric chars
@@ -125,24 +149,57 @@ if __name__ == '__main__':
 		ctypes.windll.user32.MessageBoxW(0, f'Stale file list: {stringdate}\nReplace SmartList.txt in {smartlistPath}', 'Stale File List')
 		pyperclip.copy(smartlistPath)
 
-	foundLines = []
+	foundLines:List[str] = []
 	for readline in searchLines:
 		line = CleanText(readline)
 		if searchTerms in line.lower() or searchTerms.rstrip() in line.lower() or searchTerms.lstrip() in line.lower():
 			foundLines.append(line)
 	
+	headerCell:str = str()
+	catalogCell:str = str()
+	salesCell:str = str()
+
+	# print a warning if the file searched has 0 records
+	catalogCell += '<h2>Server Catalog</h2>'
+	if len(searchLines) == 0:
+		catalogCell += '<p>INVALID</p>'
+	else:
+		for l in foundLines:
+			catalogCell += f'{l}<br />'
+
+		# add lines from sales file
+		salesCell += '<h2>Sales Data</h2>'
+		if len(salesFileLines) > 0:
+			for l in salesFileLines:
+				salesCell += f'{l}<br />'
+		else:
+			salesCell += '(no sales data found)'
+
+	googleFontName:str = GetGoogleFontName()
 	with open(outputPath, 'w') as outfile:
+		outfile.write(f'<html>')
+		outfile.write(f'<head>')
+		outfile.write(f'<link rel="stylesheet" href="https://fonts.googleapis.com/css?family={googleFontName}">')
+		outfile.write(f'<style>')
+		outfile.write(f'body {{ font-family: "{googleFontName}", sans-serif; width: 100% }}')
+		outfile.write('td { width: 50%; vertical-align: top; min-width: 500px; }')
+		outfile.write(f'</style>')
+		outfile.write(f'</head><body>')
+
 		outfile.write(f'Searching: <a href="file://{searchFile}" target="_new">{searchFile}</a>&nbsp;({str(filedate)[:10]})<br />')
 		outfile.write(f'Search Terms: {searchTerms.strip()}<br />')
-		outfile.write(f'Found {len(foundLines)} matches in {len(searchLines)} records<br />&nbsp;<br />')
-		
-		# print a warning if the file searched has 0 records
-		if len(searchLines) == 0:
-			outfile.write('<p>INVALID</p>')
-		else:
-			for l in foundLines:
-				outfile.write(f'{l}<br />')
-	
+		outfile.write(f'Found {len(foundLines)} matches in {len(searchLines)} records<br />') 
+		outfile.write(f'Font Name: {googleFontName}<br />')
+		outfile.write(f'&nbsp;<br />')
+
+		outfile.write(f'<table><tr>')
+		outfile.write(f'<td>{catalogCell}</td>')
+		outfile.write(f'<td>{salesCell}</td>')
+		outfile.write(f'</tr></table>')
+		outfile.write(f'</body></html>')
+
+
+				
 	# everything output file
 	if (len(EVERYTHING_COMMAND_LINE_PATH) > 0):
 		command = f'{EVERYTHING_COMMAND_LINE_PATH} -r {searchTerms} > {everythingOutputPath}'

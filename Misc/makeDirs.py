ab734d5a692ba595e177a6d8f869b46aa305ccfd
Bob Whitley <bobwhitley@gmail.com>
2025-12-13 06:05:15 -0600

    Backup by startup.bat

--- Misc/makeDirs.py
+++ Misc/makeDirs.py
@@ -6,9 +6,16 @@ def daterange(start_date, end_date):
     for n in range(int((end_date - start_date).days)):
         yield start_date + timedelta(n)
 
-start_date = date(2025, 11, 1)
-end_date = date(2026, 1, 31)
+i = 0
+start_date = date(2026, 2, 1)
+end_date = date(2026, 4, 30)
 for single_date in daterange(start_date, end_date + timedelta(days=1)):
+
+    i += 1
+    if i > 1000:
+        print('This thing is out of control! Breaking out of loop.')
+        break
+
     month_dir = single_date.strftime("%Y-%m")
     if not isdir(month_dir):
         mkdir(month_dir)
